4acf84f54849e8ded1b67fb804e83a2a839a0d98
Joe Robertazzi <robertazzijoseph@gmail.com>
2025-02-15 23:54:12 -0800

    event data

--- my-app/app/(tabs)/swipe.tsx
+++ my-app/app/(tabs)/swipe.tsx
@@ -2,42 +2,83 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SwipeCard from '@/components/SwipeCard';
 import Button from '@/components/Button';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// Database get
+import { db } from '../utils/firebase/firebase.utils';
+import { collection, getDocs, Firestore } from 'firebase/firestore';
 
 const cardData = [
   {
-    id: 1,
+    id: "1",
     imageUrl: 'https://picsum.photos/400/600',
     title: 'First Card',
-    description: 'first card example'
+    description: 'first card example',
+    date: '2025-01-01',
+    location: 'New York, NY'
   },
   {
-    id: 2,
+    id: "2",
     imageUrl: 'https://picsum.photos/400/500',
     title: 'Second Card',
-    description: 'second card example'
+    description: 'second card example',
+    date: '2025-01-02',
+    location: 'Los Angeles, CA'
   },
   {
-    id: 3,
+    id: "3",
     imageUrl: 'https://picsum.photos/400/440',
     title: 'Third Card',
-    description: 'third card example'
+    description: 'third card example',
+    date: '2025-01-03',
+    location: 'Chicago, IL'
   },
   {
-    id: 4,
+    id: "4",
     imageUrl: 'https://picsum.photos/400/128',
     title: '4 Carddada',
-    description: 'third card example'
+    description: 'third card example',
+    date: '2025-01-04',
+    location: 'San Francisco, CA'
   },
   {
-    id: 5,
+    id: "5",
     imageUrl: 'https://picsum.photos/400/893',
     title: '5 Cardwdw',
-    description: 'third cadsadard example'
+    description: 'third cadsadard example',
+    date: '2025-01-05',
+    location: 'New York, NY'
   }
 ];
 
+
 export default function SwipeScreen() {
+  // Get events from database
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const eventsCollectionRef = collection(db as Firestore, 'events');
+        const querySnapshot = await getDocs(eventsCollectionRef);
+        querySnapshot.forEach((doc) => {
+          cardData.push({
+            id: doc.id,
+            imageUrl: doc.data().imageUrl,
+            title: doc.data().title,
+            description: doc.data().description,
+            date: doc.data().date,
+            location: doc.data().location
+          });
+          console.log('Event:', { id: doc.id, ...doc.data() });
+        });
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleSwipeLeft = () => {
@@ -72,6 +113,9 @@ export default function SwipeScreen() {
               imageUrl={cardData[currentIndex + 1].imageUrl}
               title={cardData[currentIndex + 1].title}
               description={cardData[currentIndex + 1].description}
+              date={cardData[currentIndex + 1].date}
+              location={cardData[currentIndex + 1].location}
+              id={cardData[currentIndex + 1].id}
               style={{ 
                 transform: [{ scale: 0.95 }] 
               }}
@@ -84,6 +128,9 @@ export default function SwipeScreen() {
             imageUrl={cardData[currentIndex].imageUrl}
             title={cardData[currentIndex].title}
             description={cardData[currentIndex].description}
+            date={cardData[currentIndex].date}
+            location={cardData[currentIndex].location}
+            id={cardData[currentIndex].id}
             onSwipeLeft={handleSwipeLeft}
             onSwipeRight={handleSwipeRight}
             currentIndex={currentIndex}
